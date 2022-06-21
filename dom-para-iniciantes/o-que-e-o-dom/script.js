c8d9fcfd7ced462a05e32ee95d5a785ba2aa2435
Giovany César <gvncesar@gmail.com>
2022-06-21 18:47:48 -0300

    Initial commit

---
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -0,0 +1,26 @@
+/*window.alert ("Oi");
+
+const h1Selecionado = document.querySelector ("h1");
+const h1Classes = h1Selecionado.classList
+
+function callbackh1 () {
+    console.log ("Clicou em", h1Selecionado.innerText)
+}
+
+h1Selecionado.addEventListener ("click", callbackh1) */
+
+const h = window.location.href
+
+console.log (h)
+
+const n = window.navigator.language
+
+console.log (n)
+
+const l = window.innerWidth
+
+console.log (l)
+
+const h1Selecionado = document.querySelector (".ativo");
+
+console.log (h1Selecionado)
