c838cc713b0b57b8af115b199a08ebc2f6a63aa7
Giovany César <gvncesar@gmail.com>
2022-12-16 08:59:10 -0300

    selecionar o dom

---
+++ dom-para-iniciantes/selecionar-o-dom/script.js
@@ -0,0 +1,59 @@
+/*const animais = document.getElementById('animais');
+console.log (animais);
+
+const gridSection = document.getElementsByClassName('grid-section')
+console.log (gridSection[1]);
+
+const primeiraTag = document.getElementsByTagName('h1')
+console.log (primeiraTag[2]);
+
+const primeiraLi = document.querySelector('li')
+console.log (primeiraLi);
+
+const primeiraUl = document.querySelector('ul')
+console.log (primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]')
+console.log (linkInterno);
+
+const animaisImg = document.querySelectorAll('.animais-lista img')
+console.log (animaisImg[2]);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section')
+const gridSectionNODE = document.querySelectorAll('.grid-section')
+
+primeiraUl.classList.add('grid-section')
+
+//console.log (gridSectionHTML);
+//console.log (gridSectionNODE);
+
+gridSectionNODE.forEach(function(item, index){
+console.log(item)
+})
+
+ const arrayGrid = Array.from(gridSectionHTML);
+
+ arrayGrid.forEach(function(item){
+    console.log(item)
+    }) */
+
+const todasImg = document.getElementsByTagName('img')
+
+console.log (todasImg)
+
+
+const imagem = document.querySelectorAll('img[src^="img/imagem"]')
+console.log (imagem);
+
+const linksInterno = document.querySelectorAll('[href^="#"]')
+console.log (linksInterno);
+
+const primeiraH2 = document.querySelector('.animais-descricao h2')
+console.log (primeiraH2);
+
+const ultimop = document.querySelectorAll('p')
+console.log (ultimop[ultimop.length - 1]);
+
+const paragrafos = document.getElementsByTagName('p')
+console.log (paragrafos[paragrafos.length - 1]);
+
